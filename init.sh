82885a91a5f03586e7bb05a929a52b2dee3848aa
Akim Gubanov <akim.gubanov@gmail.com>
2021-03-30 00:43:56 +0300

    fixes

--- init.sh
+++ init.sh
@@ -8,4 +8,5 @@ sudo apt-get upgrade -y
 sudo apt-get install gnome-tweaks -y
 sudo apt-get install dconf-editor -y
 sudo apt-get install ubuntu-restricted-extras -y
-sudo apt-get install curl mc htop git make -y
+sudo apt-get install curl wget mc htop git make -y
+sudo apt-get install i3 i3status dunst rofi -y
