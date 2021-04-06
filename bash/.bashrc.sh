6a27fabb5265375165bc66227c3be46912e9cad0
Akim Gubanov <6378353+mutan@users.noreply.github.com>
2021-04-06 19:20:47 +0300

    fixes

--- bash/.bashrc.sh
+++ bash/.bashrc.sh
@@ -1,5 +1,19 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
 
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
 
+# Aliases and functions in separate file
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+if [ -f ~/.bash_functions ]; then
+    source ~/.bash_functions
+fi
 
 # Colours
 color_reset='\e[0m'
@@ -36,6 +50,33 @@ color_purple_bg='\e[45m' # Purple Background
 color_cyan_bg='\e[46m' # Cyan Background
 color_white_bg='\e[47m' # White Background
 
+# executed before printing of each primary prompt (PS1)
+PROMPT_COMMAND='history -a'
+
+# exclude from history
+HISTIGNORE="ls:ps:history"
+
+# add date before each history record
+HISTTIMEFORMAT="%h %d %H:%M:%S "
+
+# don't put duplicate lines or lines starting with space in the history.
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# history length
+HISTSIZE=10000
+HISTFILESIZE=10000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+
+
+
+
 # Prompt colours
 pointerC="${color_white}"
 pathC="${color_cyan}"
@@ -56,20 +97,34 @@ gitBranch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-export PS1="${pathC}\w ${gitC}\$(gitBranch) ${pointerC}\$${normalC} "
+export PS1="${pathC}\w \u@\h ${gitC}\$(gitBranch) ${pointerC}\$${normalC} "
 
 
 
-# Aliases and functions in separate file
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
+
+
+
+
+
+
+
+
+
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
 fi
-
-if [ -f ~/.bash_functions ]; then
-    source ~/.bash_functions
-fi
-
-
-
-
-
