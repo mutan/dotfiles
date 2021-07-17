e6f9272c575f3f645892d4e73b332ce4f7e0e13c
Akim Gunanov <akim.gubanov@gmail.com>
2021-07-17 14:19:33 +0300

    Add classic option to snap-install.sh

--- scripts/snap-install.sh
+++ scripts/snap-install.sh
@@ -3,18 +3,23 @@
 sudo apt update
 
 function install {
-    which $1 &> /dev/null
 
+    which $1 &> /dev/null
     if [ $? -ne 0 ]; then
-        echo "Installing: ${1}..."
-        sudo snap install ${1}
+        if [ $2 = "classic" ]; then
+            echo "Installing: ${1}..."
+            sudo snap install --classic ${1}
+        else
+            echo "Installing: ${1}..."
+            sudo snap install ${1}
+        fi
     else
         echo "Already installed: ${1}"
     fi
 }
 
-install "--classic code"
-install "--classic phpstorm"
+install code classic
+install phpstorm classic
 install postman
 install teams
 install telegram-desktop
