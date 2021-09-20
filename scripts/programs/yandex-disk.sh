0a1005747b7ab797df641ad0497ad0dfc76dfa9c
Akim Gubanov <6378353+mutan@users.noreply.github.com>
2021-09-20 15:22:50 +0300

    Изменил установку yandex-disc

--- scripts/programs/yandex-disk.sh
+++ scripts/programs/yandex-disk.sh
@@ -7,11 +7,7 @@ which yandex-disk &> /dev/null
 if [ $? -ne 0 ]; then
     echo "Installing: yandex-disk"
     cd ~
-    wget -O YANDEX-DISK-KEY.GPG http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG
-    sudo apt-key add YANDEX-DISK-KEY.GPG
-    echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" >> /etc/apt/sources.list.d/yandex-disk.list
-    sudo apt-get update
-    sudo apt-get install -y yandex-disk
+    echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install -y yandex-disk
 else
     echo "Yandex-disk already installed."
 fi
